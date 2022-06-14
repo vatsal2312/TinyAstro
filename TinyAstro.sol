06c48f6a9acf9ebad9e45529cf2c9db39b05425e
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2022-06-14 12:24:46 -0400

    Add files via upload

---
+++ TinyAstro.sol
@@ -0,0 +1,345 @@
+// SPDX-License-Identifier: MIT
+//.___________. __  .__   __. ____    ____      ___           _______..___________..______        ______
+//|           ||  | |  \ |  | \   \  /   /     /   \         /       ||           ||   _  \      /  __  \
+//`---|  |----`|  | |   \|  |  \   \/   /     /  ^  \       |   (----``---|  |----`|  |_)  |    |  |  |  |
+//    |  |     |  | |  . `  |   \_    _/     /  /_\  \       \   \        |  |     |      /     |  |  |  |
+//    |  |     |  | |  |\   |     |  |      /  _____  \  .----)   |       |  |     |  |\  \----.|  `--'  |
+//    |__|     |__| |__| \__|     |__|     /__/     \__\ |_______/        |__|     | _| `._____| \______/
+
+pragma solidity >=0.8.1 <0.9.0;
+
+import "./erc721a/extensions/ERC721AQueryable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
+import "./ILeaseController.sol";
+
+error MintingNotStarted();
+error IncorrectTierAttempt();
+error MaxSupplyExceeded();
+error InvalidMintAmount();
+error CallerIsAContract();
+error IncorrectFundsAmount();
+error PublicMintIsNotActivated();
+error IncorrectAddress();
+error InvalidCoupon();
+error PriceIsTooLow();
+error TokenIsStaked();
+
+contract TinyAstro is ERC721AQueryable, ERC2981, Ownable, ReentrancyGuard {
+  string public baseURI;
+  string public hiddenMetadataURI = "ipfs://QmXBcCF8UzbJpNtXksgHZBoa2S663o5WAXvHk7P2R1LNRx/hidden.json";
+
+  uint256 public price = 0.01 ether;
+  uint256 public publicSalePrice = 0.02 ether;
+  uint256 public constant maxSupply = 100;
+
+  // on deploy make it private;
+  address public pubSigner = 0x2D038458B6bD28e75BffcaE90E1B59F8aDB7FEE6;
+
+  //Supporting tiers 1,2,3,4( 3 - raffle, 4 - public sale )
+  struct Config {
+    uint8 maxMint;
+    uint8 tierNumber;
+    bool isActive;
+  }
+  Config private mintConfig;
+
+  address private leaseController;
+
+  constructor() ERC721A("TinyAstro", "TA") {
+    mintConfig = Config({maxMint: 0, tierNumber: 0, isActive: false});
+  }
+
+  // ==== MODIFIERS ==== //
+  modifier mintGeneralCompliance(uint256 _mintAmount, uint8 _tierNumber) {
+    // validate contract is active
+    // isActive is true
+    if (!mintConfig.isActive) {
+      revert MintingNotStarted();
+    }
+
+    // validate user tier number
+    // tier should be equal to current config tier
+    if (_tierNumber != mintConfig.tierNumber) {
+      revert IncorrectTierAttempt();
+    }
+
+    // validate mintAmount
+    // should not exceed max allowed mint amount from config per msg sender tier
+    // also validating here that maxMint from configs for current tier > 0
+    if (_mintAmount > mintConfig.maxMint) {
+      revert InvalidMintAmount();
+    }
+
+    // caller is user
+    if (tx.origin != msg.sender) {
+      revert CallerIsAContract();
+    }
+
+    // validate totalSupply
+    // mintAmount + all minted tokens should be less or equal than maxSupply
+    if (_totalMinted() + _mintAmount > maxSupply) {
+      revert MaxSupplyExceeded();
+    }
+
+    // validate wallet for previous purchases
+    // we allow to mint only once per tier
+    uint64 tierMintedMask = uint64(1 << (_tierNumber - 1));
+    uint64 aux = _getAux(msg.sender);
+    if (aux & tierMintedMask > 0) {
+      revert InvalidMintAmount();
+    }
+
+    _;
+
+    _setAux(msg.sender, aux | tierMintedMask);
+  }
+
+  /**
+   * @dev See {IERC165-supportsInterface}.
+   */
+  function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, ERC2981) returns (bool) {
+    return ERC721A.supportsInterface(interfaceId) || ERC2981.supportsInterface(interfaceId);
+  }
+
+  // ==== MAIN FUNCTIONS ==== //
+  // Mint Functions //
+
+  function mintTier(
+    uint256 _mintAmount,
+    bytes32 _r,
+    bytes32 _s,
+    uint256 _v,
+    uint8 _tierNumber,
+    uint8 _maxMintAmount
+  ) external payable mintGeneralCompliance(_mintAmount, _tierNumber) nonReentrant {
+    // validate msg.value
+    // should be == publicSalePrice by quantity of minting tokens
+    if (msg.value < (price * _mintAmount)) {
+      revert IncorrectFundsAmount();
+    }
+
+    // validate couponCode with assigned tier number and max mint amount
+    bytes32 digest = keccak256(abi.encode(_tierNumber, _maxMintAmount, msg.sender));
+    if (!_isCouponValid(digest, _r, _s, _v)) {
+      revert InvalidCoupon();
+    }
+
+    if (_mintAmount > _maxMintAmount) {
+      revert InvalidMintAmount();
+    }
+
+    _safeMint(msg.sender, _mintAmount);
+  }
+
+  function mintPublic(uint256 _mintAmount) external payable mintGeneralCompliance(_mintAmount, 4) nonReentrant {
+    // validate msg.value
+    // should be == publicSalePrice by quantity of minting tokens
+    if (msg.value < (publicSalePrice * _mintAmount)) {
+      revert IncorrectFundsAmount();
+    }
+
+    _safeMint(msg.sender, _mintAmount);
+  }
+
+  function burn(uint256 _tokenId) external {
+    _burn(_tokenId, true);
+  }
+
+  // Public View Functions //
+
+  function tokenURI(uint256 _tokenId) public view virtual override(ERC721A) returns (string memory) {
+    string memory _tokenURI = super.tokenURI(_tokenId);
+    return bytes(_tokenURI).length != 0 ? _tokenURI : hiddenMetadataURI;
+  }
+
+  function getMintData(address _owner)
+    public
+    view
+    returns (
+      uint256,
+      uint256,
+      uint256
+    )
+  {
+    return (numberMinted(_owner), totalSupply(), mintConfig.tierNumber);
+  }
+
+  function numberMinted(address owner) public view returns (uint256) {
+    return _numberMinted(owner);
+  }
+
+  //override renounceOwnership
+  function renounceOwnership() public override onlyOwner {}
+
+  // Setters Only Owner //
+  function airdrop(uint256 _mintAmount, address _receiver) external onlyOwner {
+    // validate totalSupply
+    // mintAmount + all minted tokens should be less or equal than maxSupply
+    if (_totalMinted() + _mintAmount > maxSupply) {
+      revert MaxSupplyExceeded();
+    }
+
+    // validate mintAmount
+    // should be > 0
+    // should not exceed 50 tokens
+    if (_mintAmount == 0 || _mintAmount > 50) {
+      revert InvalidMintAmount();
+    }
+
+    _safeMint(_receiver, _mintAmount);
+  }
+
+  function setBaseURI(string memory _baseURIPrefix) external onlyOwner {
+    baseURI = _baseURIPrefix;
+  }
+
+  function setPrice(uint256 _price) external onlyOwner {
+    //validate price to make sure its not less than 0.01 eth
+    if (_price < 10000000000000000) {
+      revert PriceIsTooLow();
+    }
+    price = _price;
+  }
+
+  function setPublicSalePrice(uint256 _price) external onlyOwner {
+    //validate price to make sure its not less than 0.01 eth
+    if (_price < 10000000000000000) {
+      revert PriceIsTooLow();
+    }
+    publicSalePrice = _price;
+  }
+
+  function setHiddenMetadataURI(string calldata _hiddenMetadataURI) external onlyOwner {
+    hiddenMetadataURI = _hiddenMetadataURI;
+  }
+
+  function pauseContract() external onlyOwner {
+    mintConfig.tierNumber = 0;
+    mintConfig.maxMint = 0;
+    mintConfig.isActive = false;
+  }
+
+  function setActiveTier(uint8 _tierNumber, uint8 _maxMint) external onlyOwner {
+    mintConfig.tierNumber = _tierNumber;
+    mintConfig.maxMint = _maxMint;
+    mintConfig.isActive = true;
+  }
+
+  function setGlobalConfig(
+    uint8 _maxMint,
+    uint8 _tierNumber,
+    bool _isActive
+  ) external onlyOwner {
+    mintConfig = Config({maxMint: _maxMint, tierNumber: _tierNumber, isActive: _isActive});
+  }
+
+  function setPubSigner(address _pubSigner) external onlyOwner {
+    pubSigner = _pubSigner;
+  }
+
+  function withdraw() external onlyOwner {
+    (bool success, ) = msg.sender.call{value: address(this).balance}("");
+    require(success, "Transfer failed.");
+  }
+
+  function showGlobalConfig()
+    external
+    view
+    returns (
+      uint8 maxMint,
+      uint8 tierNumber,
+      bool isActive
+    )
+  {
+    return (mintConfig.maxMint, mintConfig.tierNumber, mintConfig.isActive);
+  }
+
+  function getOwnershipData(uint256 tokenId) external view returns (TokenOwnership memory) {
+    return _ownershipOf(tokenId);
+  }
+
+//  function firstTokenURI(address owner) external view onlyOwner returns (string memory) {
+//    if (owner == address(0)) revert IncorrectAddress();
+//
+//    uint256 curr = 0;
+//
+//    unchecked {
+//      while (curr < _currentIndex) {
+//        TokenOwnership memory ownership = _ownerships[curr];
+//        if (ownership.addr == owner && !ownership.burned) {
+//          break;
+//        }
+//        curr++;
+//      }
+//    }
+//
+//    return curr == _currentIndex ? "" : tokenURI(curr);
+//  }
+
+  // EIP-2981 //
+
+  function setDefaultRoyalty(address receiver, uint96 feeNumerator) external onlyOwner {
+    _setDefaultRoyalty(receiver, feeNumerator);
+  }
+
+  function deleteDefaultRoyalty() external onlyOwner {
+    _deleteDefaultRoyalty();
+  }
+
+  function setTokenRoyalty(
+    uint256 tokenId,
+    address receiver,
+    uint96 feeNumerator
+  ) external onlyOwner {
+    _setTokenRoyalty(tokenId, receiver, feeNumerator);
+  }
+
+  function resetTokenRoyalty(uint256 tokenId) external onlyOwner {
+    _resetTokenRoyalty(tokenId);
+  }
+
+  // Internal Functions //
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  function _isCouponValid(
+    bytes32 digest,
+    bytes32 _r,
+    bytes32 _s,
+    uint256 _v
+  ) internal view returns (bool) {
+    address signer = ecrecover(digest, uint8(_v), _r, _s);
+    if (signer == address(0)) {
+      revert IncorrectAddress();
+    }
+    return signer == pubSigner;
+  }
+
+  function _beforeTokenTransfers(
+    address from,
+    address, /* to */
+    uint256 startTokenId,
+    uint256 quantity
+  ) internal view override {
+    if (from != address(0) && quantity == 1) {
+      // Before transfer or burn, verify the token is not staked.
+      if (leaseController != address(0) && ILeaseController(leaseController).isTokenStaked(startTokenId)) {
+        revert TokenIsStaked();
+      }
+    }
+  }
+
+  // Lease //
+
+  function _setLeaseController(address _leaseController) internal {
+    leaseController = _leaseController;
+  }
+
+  function setLeaseController(address _leaseController) external onlyOwner {
+    _setLeaseController(_leaseController);
+  }
+}
---
+++ TinyAstroLeaseController.sol
@@ -0,0 +1,269 @@
+// SPDX-License-Identifier: MIT
+//.___________. __  .__   __. ____    ____      ___           _______..___________..______        ______
+//|           ||  | |  \ |  | \   \  /   /     /   \         /       ||           ||   _  \      /  __  \
+//`---|  |----`|  | |   \|  |  \   \/   /     /  ^  \       |   (----``---|  |----`|  |_)  |    |  |  |  |
+//    |  |     |  | |  . `  |   \_    _/     /  /_\  \       \   \        |  |     |      /     |  |  |  |
+//    |  |     |  | |  |\   |     |  |      /  _____  \  .----)   |       |  |     |  |\  \----.|  `--'  |
+//    |__|     |__| |__| \__|     |__|     /__/     \__\ |_______/        |__|     | _| `._____| \______/
+
+pragma solidity >=0.8.1 <0.9.0;
+
+import "./ILeaseController.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+error NotATokenOwner();
+error NotTheLessor();
+error InvalidLessee();
+error InvalidLease();
+error LeaseAlreadySigned();
+error TokenIsStaked();
+error HasActiveLease();
+error InvalidLeaseDuration();
+error IncorrectFundsAmount();
+error InvalidEarningFraction();
+
+/*
+ * TinyAstro lease controller
+ */
+contract TinyAstroLeaseController is Ownable, ReentrancyGuard {
+  // Address of TA contract
+  address tokenContract;
+
+  struct Lease {
+    uint256 price; // If price is 0 and lessee is not ZERO then it's a gifted lease
+    address lessee; // If lessee is ZERO address then anyone can sign the lease
+    uint64 listedTime;
+    uint64 duration;
+    uint64 signedTime;
+  }
+
+  event LeaseCreated(
+    uint256 indexed tokenId,
+    address indexed lessor,
+    address indexed lessee,
+    uint256 duration,
+    uint256 price
+  );
+
+  event LeaseUpdated(
+    uint256 indexed tokenId,
+    address indexed lessor,
+    address indexed lessee,
+    uint256 duration,
+    uint256 price
+  );
+
+  event LeaseCancelled(uint256 indexed tokenId, address indexed lessor);
+
+  event LeaseSigned(uint256 indexed tokenId, address indexed lessor, address indexed lessee);
+
+  mapping(uint256 => Lease) private leaseByToken;
+
+  mapping(address => uint256) private tokenByLessee;
+
+  uint64 public maxLeaseDuration = 31536000;
+
+  // Basic unit points (out of 10000) of how much listing price goes to the lessor, default to 80%
+  uint16 public earningFraction = 8000;
+
+  uint16 private constant FEE_DENOMINATOR = 10000;
+
+  constructor(address _tokenContract) {
+    tokenContract = _tokenContract;
+  }
+
+  // External
+
+  /**
+   *  Create a lease that can be later signed by another party.
+   *  A lease is considered a gift when listingPrice is 0 and lessee is not zero address.
+   *  A gifted lease will be signed upon creation to avoid further gas cost.
+   */
+  function createLease(
+    uint256 tokenId,
+    uint64 duration,
+    uint256 listingPrice,
+    address lessee
+  ) external {
+    address tokenOwner = IERC721(tokenContract).ownerOf(tokenId);
+
+    if (tokenOwner != msg.sender) revert NotATokenOwner();
+
+    if (duration == 0 || duration > maxLeaseDuration) revert InvalidLeaseDuration();
+
+    if (lessee != address(0)) {
+      // Lessee cannot be token owner
+      if (lessee == tokenOwner) {
+        revert InvalidLessee();
+      }
+
+      // Cannot gift to a lesse with active lease with other tokens
+      if (listingPrice == 0 && isActiveLessee(lessee)) {
+        revert InvalidLessee();
+      }
+    }
+
+    Lease storage lease = leaseByToken[tokenId];
+
+    if (_isLeaseActive(lease) || _isLeaseListed(lease)) {
+      revert TokenIsStaked();
+    }
+
+    uint64 _now = uint64(block.timestamp);
+
+    lease.listedTime = _now;
+    lease.price = listingPrice;
+    lease.duration = duration;
+    lease.lessee = lessee;
+
+    emit LeaseCreated(tokenId, msg.sender, lessee, duration, listingPrice);
+
+    if (listingPrice == 0 && lessee != address(0)) {
+      // The lease is considered a gift and will be signed automatically
+      lease.signedTime = _now;
+      tokenByLessee[lessee] = tokenId;
+
+      emit LeaseSigned(tokenId, msg.sender, lessee);
+    }
+  }
+
+  /**
+   *  Update a lease that is listed but not signed.
+   *  @dev When listingPrice is 0 and lessee is not zero address, the lease is considered a gift
+   *  and will be signed automatically.
+   */
+  function updateLease(
+    uint256 tokenId,
+    uint64 duration,
+    uint256 listingPrice,
+    address lessee
+  ) external verifyLeaseUpdatable(tokenId) {
+    if (lessee == msg.sender) revert InvalidLessee();
+    if (duration == 0 || duration > maxLeaseDuration) revert InvalidLeaseDuration();
+
+    Lease storage lease = leaseByToken[tokenId];
+    lease.duration = duration;
+    lease.price = listingPrice;
+    lease.lessee = lessee;
+
+    emit LeaseUpdated(tokenId, msg.sender, lessee, duration, listingPrice);
+
+    if (listingPrice == 0 && lessee != address(0)) {
+      lease.signedTime = uint64(block.timestamp);
+      tokenByLessee[lessee] = tokenId;
+      emit LeaseSigned(tokenId, msg.sender, lessee);
+    }
+  }
+
+  /**
+   *  Cancel a listed or gifted lease.
+   */
+  function cancelLease(uint256 tokenId) external verifyLeaseUpdatable(tokenId) {
+    delete leaseByToken[tokenId];
+    emit LeaseCancelled(tokenId, msg.sender);
+  }
+
+  /**
+   *  Sign a listed lease
+   */
+  function signLease(uint256 tokenId) external payable nonReentrant {
+    Lease storage lease = leaseByToken[tokenId];
+
+    if (lease.listedTime == 0) revert InvalidLease();
+    if (lease.signedTime != 0) revert LeaseAlreadySigned();
+    if (msg.value != lease.price) revert IncorrectFundsAmount();
+    if (isActiveLessee(msg.sender)) revert HasActiveLease();
+
+    address tokenOwner = IERC721(tokenContract).ownerOf(tokenId);
+
+    if (msg.sender == tokenOwner) revert InvalidLessee();
+    if (lease.lessee != address(0) && lease.lessee != msg.sender) revert InvalidLessee();
+
+    lease.lessee = msg.sender;
+    lease.signedTime = uint64(block.timestamp);
+
+    tokenByLessee[msg.sender] = tokenId;
+
+    if (lease.price > 0) {
+      uint256 amountToLessor = (lease.price * earningFraction) / FEE_DENOMINATOR;
+      Address.sendValue(payable(tokenOwner), amountToLessor);
+    }
+
+    emit LeaseSigned(tokenId, tokenOwner, msg.sender);
+  }
+
+  /**
+   *  Returns whether the given user is an active lessee, i.e. signed a lease that has not expired.
+   */
+  function isActiveLessee(address user) public view returns (bool) {
+    if (user == address(0)) revert InvalidLessee();
+
+    uint256 tokenId = tokenByLessee[user];
+
+    Lease storage lease = leaseByToken[tokenId];
+
+    return _isLeaseActive(lease) && lease.lessee == user;
+  }
+
+  function isTokenStaked(uint256 tokenId) external view returns (bool) {
+    return _isLeaseListed(leaseByToken[tokenId]) || _isLeaseActive(leaseByToken[tokenId]);
+  }
+
+  function getLease(uint256 tokenId) external view returns (Lease memory) {
+    return leaseByToken[tokenId];
+  }
+
+  function withdraw() external onlyOwner {
+    Address.sendValue(payable(msg.sender), address(this).balance);
+  }
+
+  function setEarningFraction(uint16 value) external onlyOwner {
+    if (value > FEE_DENOMINATOR) revert InvalidEarningFraction();
+    earningFraction = value;
+  }
+
+  function setMaxLeaseDuration(uint64 value) external onlyOwner {
+    if (value == 0) revert InvalidLeaseDuration();
+    maxLeaseDuration = value;
+  }
+
+  // Internal
+
+  /**
+   *  Returns whether a given lease is been listed
+   */
+  function _isLeaseListed(Lease storage lease) internal view returns (bool) {
+    return lease.listedTime > 0 && lease.signedTime == 0;
+  }
+
+  /**
+   *  Returns whether a given lease is signed and not expired
+   */
+  function _isLeaseActive(Lease storage lease) internal view returns (bool) {
+    return lease.signedTime > 0 && lease.lessee != address(0) && block.timestamp < (lease.signedTime + lease.duration);
+  }
+
+  // Modifiers
+
+  /**
+   *  A lease can be updated when:
+   *  1. It's listed but not signed
+   *  2. It's a gift to someone else
+   */
+  modifier verifyLeaseUpdatable(uint256 tokenId) {
+    address tokenOwner = IERC721(tokenContract).ownerOf(tokenId);
+
+    Lease storage lease = leaseByToken[tokenId];
+
+    if (tokenOwner != msg.sender) revert NotATokenOwner();
+    if (lease.listedTime == 0) revert InvalidLease();
+
+    // Owner can cancel a gifted lease
+    if (lease.signedTime != 0 && lease.price > 0) revert LeaseAlreadySigned();
+
+    _;
+  }
+}
